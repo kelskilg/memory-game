b68c71dec8ba15c2caecf8826c3b6d3cba0dffe7
Kelsey Kilgore <kelseykilgore@Kelseys-MacBook-Pro.local>
2019-09-08 21:51:06 -0600

    added game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-console.log("Up and running!");
-
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "diamonds",
+		cardImage : "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
+
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+  		alert("You found a match!");
+	} else {
+  		alert("Sorry, try again.");
+	}
 }
-}
+
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
-	checkForMatch();
+	cardsInPlay.push(cards);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("cardImage " + cards[cardId].cardImage)
+	console.log("suit " + cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
 
 flipCard(0);
